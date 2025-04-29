e468326b34c90e9f12accc37b242a3baf9b5b55f
Ramesh Edirisinghe <e2145434@bit.uom.lk>
2025-04-29 14:22:22 +0530

    update report section

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { saveAs } from 'file-saver';
+import { MaterialModule } from '../../material.module';
 
 
 @Component({
   selector: 'app-report',
+  imports: [ MaterialModule],
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.scss']
 })
 export class ReportComponent {
+  isLoading = false;
+  lastDownloaded: string | null = null;
 
+  constructor(private http: HttpClient) {}
+
+  downloadLastMonthReport() {
+    this.downloadReport('/report/last-month', 'last_month_report.xlsx');
+  }
+
+  downloadLast10DaysReport() {
+    this.downloadReport('/report/last-10-days', 'last_10_days_employees.xlsx');
+  }
+
+  private downloadReport(endpoint: string, filename: string) {
+    this.isLoading = true;
+    this.lastDownloaded = filename;
+
+    this.http.get(endpoint, {
+      responseType: 'blob',
+      observe: 'response'
+    }).subscribe(response => {
+      const blob = new Blob([response.body!], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      saveAs(blob, filename);
+      this.isLoading = false;
+    }, error => {
+      console.error('Error downloading report:', error);
+      this.isLoading = false;
+    });
+  }
 }
